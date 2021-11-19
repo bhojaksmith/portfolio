64971eed49eaa2b13b8fff93e8301dc317c97630
Smith Bhojak <99sbhojak@gmail.com>
2021-11-19 14:38:23 +0530

    Added Portfolio Page

--- portfolio.js
+++ portfolio.js
@@ -2,11 +2,11 @@ function about(){
      window.open("index.html","_self");
      swal("I am still working on this one!");
  }
-document.body.style.cursor='pointer';
- swal({
-    title: "You caught me!",
-    text: "I am Still Working on this one",
-    type: "fail",
-    }).then(function() {
-        document.body.style.cursor = "none";
-    });
+// document.body.style.cursor='pointer';
+//  swal({
+//     title: "You caught me!",
+//     text: "I am Still Working on this one",
+//     type: "fail",
+//     }).then(function() {
+//         document.body.style.cursor = "none";
+//     });
