ece498fbd1446ca84d7afcfc3484e92ee54c949e
Smith Bhojak <99sbhojak@gmail.com>
2022-01-08 00:44:39 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -9,18 +9,19 @@
 // });
 
 let smithPhoto = document.querySelector('.smithPhoto');
-let caseImage = document.querySelector('.caseImage');
+let contactDummy = document.querySelector('.contactDummy');
 
 document.addEventListener('mousemove', function(e) {
   let xAxis = (window.innerWidth  - e.pageX) / 50;
   let yAxis = (window.innerHeight  - e.pageY) / 50;
   smithPhoto.style.transform = `translateY(${yAxis}px)  translateX(${xAxis}px)`;
-});
 
+});
 document.addEventListener('mousemove', function(e) {
-  let xAxis = (window.innerWidth / 0.5 - e.pageX) / 10;
-  let yAxis = (window.innerHeight / 0.5 - e.pageY) / 5;
-  threeD.style.transform = `rotateY(${xAxis}deg)  rotateX(${yAxis}deg)`;
+  let xAxis = (window.innerWidth  - e.pageX) / 50;
+  let yAxis = (window.innerHeight  - e.pageY) / 50;
+  contactDummy.style.transform = `translateY(${yAxis}px)  translateX(${xAxis}px)`;
+
 });
 
 function invisible(){
